291fee791ae31b162e4108b08c3e66391c6ca54e
dragonly <liyilongko@163.com>
2017-11-07 12:45:01 +0800

    refactor

--- js/index.js
+++ js/index.js
@@ -1,75 +1,87 @@
-class GameObject {
-  constructor(ctx) {
+class Background {
+  constructor(ctx, props) {
     this.ctx = ctx
+    this.dimension = props.dimension
   }
+
   draw() {
     let ctx = this.ctx
-    this.doDraw()  // implemented by sub-classes
+    ctx.fillStyle = '#2d3142'
+    ctx.fillRect(0, 0, this.dimension.width, this.dimension.height)
   }
+
+  stepState() { return true }
 }
 
-class Border extends GameObject {
+
+class Border {
   constructor(ctx, props) {
-    super(ctx)
+    this.ctx = ctx
     this.topLeft = props.topLeft
     this.dimension = props.dimension
     this.width = props.width
   }
-  doDraw() {
+
+  draw() {
     let ctx = this.ctx
     ctx.strokeStyle = '#4f5d75'
     ctx.lineWidth = this.width
     ctx.strokeRect(this.topLeft.x, this.topLeft.y, this.dimension.width, this.dimension.height)
   }
+
+  stepState() { return true }
 }
 
-class Player extends GameObject {
+
+class Player {
   constructor(ctx, props) {
-    super(ctx)
+    this.ctx = ctx
     this.center = props.center
     this.width = props.width
     this.speed = props.speed
     this.commands = {}
   }
-  doDraw() {
+
+  draw() {
     let ctx = this.ctx
     ctx.strokeStyle = '#ef8354'
     ctx.lineWidth = this.width / 2
     ctx.strokeRect(this.center.x - this.width / 4, this.center.y - this.width / 4, this.width / 2, this.width / 2)
   }
-  keyDown(key) {
-    this.commands[key] = true
-  }
-  keyUp(key) {
-    this.commands[key] = false
-  }
-  nextState() {
-    let needNextFrameUpdate = false
-    if (this.commands['ArrowUp'] || this.commands['w']) {
+
+  stepState(keys) {
+    let updated = false
+    if (keys.has('ArrowUp') || keys.has('w')) {
       this.center.y -= this.speed
-      needNextFrameUpdate = true
+      updated = true
     }
-    if (this.commands['ArrowRight'] || this.commands['d']) {
+    if (keys.has('ArrowRight') || keys.has('d')) {
       this.center.x += this.speed
-      needNextFrameUpdate = true
+      updated = true
     }
-    if (this.commands['ArrowDown'] || this.commands['s']) {
+    if (keys.has('ArrowDown') || keys.has('s')) {
       this.center.y += this.speed
-      needNextFrameUpdate = true
+      updated = true
     }
-    if (this.commands['ArrowLeft'] || this.commands['a']) {
+    if (keys.has('ArrowLeft') || keys.has('a')) {
       this.center.x -= this.speed
-      needNextFrameUpdate = true
+      updated = true
     }
-    return needNextFrameUpdate
+    console.log(keys)
+    console.log(updated)
+    return updated
   }
 }
 
-class Game extends GameObject {
+
+class Game {
   constructor(ctx, dimension) {
-    super(ctx)
     this.ctx = ctx
     this.dimension = dimension
+
+    this.background = new Background(ctx, {
+      dimension: dimension
+    })
 
     let lineWidth = 20
     this.border = new Border(ctx, {
@@ -83,6 +95,7 @@ class Game extends GameObject {
       },
       width: lineWidth
     })
+
     this.player = new Player(ctx, {
       center: {
         x: dimension.width / 2,
@@ -92,27 +105,31 @@ class Game extends GameObject {
       speed: 10
     })
     
-    this.needUpdate = true
-    this.tick = this.tick.bind(this)
+    this.boundFn = {}
+    this.boundFn.tick = this.tick.bind(this)
+    
+    this.states = {
+      downKeys: new Set(),
+      needRedraw: true
+    }
   }
-  drawBackground() {
-    let ctx = this.ctx
-    ctx.fillStyle = '#2d3142'
-    ctx.fillRect(0, 0, this.dimension.width, this.dimension.height)
-  }
-  doDraw() {
-    this.drawBackground()
+
+  draw() {
+    this.background.draw()
     this.border.draw()
     this.player.draw()
   }
+
   keyDown(key) {
-    this.player.keyDown(key)
-    this.needUpdate = true
+    this.states.downKeys.add(key)
+    // console.log(this.states.downKeys)
   }
+
   keyUp(key) {
-    this.player.keyUp(key)
-    this.needUpdate = true
+    this.states.downKeys.delete(key)
+    // console.log(this.states.downKeys)
   }
+
   checkCollision() {
     if (this.player.center.x - this.player.width / 2 < this.border.width) {
       this.player.center.x = this.border.width + this.player.width / 2
@@ -132,22 +149,33 @@ class Game extends GameObject {
     }
     return false
   }
-  nextState() {
-    let needNextFrameUpdate = this.player.nextState()
-    this.checkCollision()
-    return needNextFrameUpdate
+
+  stepState() {
+    if (this.states.downKeys.size === 0) return false
+
+    let updated = this.player.stepState(this.states.downKeys)
+    // dummy operations just to give the idea of `|=`
+    // updated |= this.border.stepState()
+    // updated |= this.background.stepState()
+    if (updated) {
+      this.checkCollision()
+    }
+    return updated
   }
+
   tick() {
-    if (this.needUpdate) {
-      this.needUpdate = this.nextState()
+    if (this.states.needRedraw) {
       this.draw()
     }
-    window.requestAnimationFrame(this.tick)
+    this.states.needRedraw = this.stepState()
+    window.requestAnimationFrame(this.boundFn.tick)
   }
+
   start() {
     this.tick()
   }
 }
+
 
 function bindKeys(game) {
   window.addEventListener('keydown', function(event) {
@@ -172,5 +200,6 @@ function init() {
   bindKeys(game)
   game.start()
 }
+
 
 init()
